b137673874cc0e584f5001db2600ea523352989e
JamesonThai <jameson.k.thai@gmail.com>
2018-05-07 10:24:01 -0700

    updates

---
+++ HTMLGenerator/messages.js
@@ -0,0 +1,5 @@
+// This is to create messages and make them seaparate call
+let messages = ["You are Great!", "Help Me", "Get Me Out Of Here"];
+
+// Export messages to another file
+export.messages=["You are Great!", "Help Me", "Get Me Out Of Here"];
---
+++ HTMLGenerator/printmessages.js
@@ -0,0 +1,4 @@
+const msgModule = require('messages.js');
+msgModule.messages.forEach((m) =&gt; {
+	console.log(m);
+})
